bf734dc5df5f2bc5ba07703e56ef76ec493f46ee
baronTommy <1675038-Tommy-baron@users.noreply.gitlab.com>
2021-10-18 23:25:54 +0900

    Fix: :art:
    Close #

--- src/cui/terminal/terminal.ts
+++ src/cui/terminal/terminal.ts
@@ -2,19 +2,29 @@ import * as inquirer from "inquirer";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 import * as inquirerAutocompletePrompt from "inquirer-search-list";
+import { table } from "table";
 import type { Config, Question } from "~/domain/type";
 inquirer.registerPrompt("search-list", inquirerAutocompletePrompt);
 
 export const clear = console.clear;
 
 export const renderTpl = (p: Pick<Config, "template">) => {
-  console.log("-----------");
+  const data = [
+    [
+      p.template
+        .replace(new RegExp("{{", "g"), "")
+        .replace(new RegExp("}}", "g"), ""),
+    ],
+  ];
+
   console.log(
-    p.template
-      .replace(new RegExp("{{", "g"), "")
-      .replace(new RegExp("}}", "g"), "")
+    table(data, {
+      header: {
+        alignment: "center",
+        content: "Your template",
+      },
+    })
   );
-  console.log("-----------");
 };
 
 export const qAndA = (p: {
