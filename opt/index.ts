831ef88f269cd9febac4e8b91e3d7de3edc5c2fe
baronTommy <1675038-Tommy-baron@users.noreply.gitlab.com>
2021-10-20 20:27:49 +0900

    Fix: :art:
    {{issue}}

--- opt/index.ts
+++ opt/index.ts
@@ -2,6 +2,8 @@ import * as github from "./plugin/github";
 import { gitmojis } from "./plugin/gitmoji";
 import { conventionalCommit } from "./setting/conventionalCommit";
 
+export type { Setting } from "~/domain/core";
+
 export const plugin = {
   gitmojis,
   github,
