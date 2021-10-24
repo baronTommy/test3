fff2c0394611495e7df7d42bb7d807ca13089ffc
baronTommy <1675038-Tommy-baron@users.noreply.gitlab.com>
2021-10-24 13:57:12 +0900

    feat: :bug: conf

--- release.config.js
+++ release.config.js
@@ -26,7 +26,6 @@ module.exports = {
     //         { type: "ci", section: "Continuous Integration", hidden: false },
     //         // https://stackoverflow.com/questions/59066245/semantic-release-add-more-sections-to-auto-generated-release-notes
     //         { type: "hoge", section: "Improvement", hidden: false },
-
     //         // その他
     //         { type: "", section: "---", hidden: false },
     //       ],
